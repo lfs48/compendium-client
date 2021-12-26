5c3a3995786766e5f2a953284bd770fdbf2c73d7
Lucas <lucasfschraier@gmail.com>
2021-12-26 13:59:01 -0500

    Implement loading state for buttons

--- src/components/atoms/button/index.tsx
+++ src/components/atoms/button/index.tsx
@@ -4,20 +4,35 @@ import * as S from './styled';
 interface ButtonProps {
     onClick: () => void;
     children: ReactNode;
+    loading?: boolean;
+    disabled?: boolean;
+    block?: boolean;
     [prop: string]: any;
 }
 
 export default function Button({
     onClick,
     children,
+    loading=false,
+    disabled=false,
+    block=false,
     ...props
 }: ButtonProps) {
     return(
         <S.Root 
+            disabled={disabled || loading}
             onClick={onClick}
+            $block={block}
             {...props}
         >
-            {children}
+            {loading &&
+                <S.LoadingContainer>
+                    <S.Spinner />
+                </S.LoadingContainer>
+            }
+            <S.Content $loading={loading}>
+                {children}
+            </S.Content>
         </S.Root>
     )
 }
--- src/components/atoms/button/styled.ts
+++ src/components/atoms/button/styled.ts
@@ -1,5 +1,51 @@
 import tw from 'tailwind-styled-components';
+import { Oval } from 'svg-loaders-react';
 
-export const Root = tw.button`
+type RootProps = {
+    $block?: boolean;
+}
+
+export const Root = tw.button<RootProps>`
     focus:outline-none
+    transition-colors
+    duration-200
+    bg-blue-400
+    hover:bg-blue-500
+    px-4
+    py-2
+    rounded
+    relative
+    font-bold
+    inline-flex
+    justify-center
+    items-center
+    disabled:bg-gray-400
+    disabled:text-gray-700
+    disabled:cursor-default
+    ${p => p.$block ? 'w-full' : ''}
+`
+
+export const LoadingContainer = tw.div`
+    absolute 
+    left-0 
+    top-0 
+    w-full 
+    h-full 
+    flex 
+    items-center 
+    justify-center
+`
+
+export const Spinner = tw(Oval)`
+    h-1/2 
+    text-blue-500 
+    stroke-current
+`
+
+type ContentProps = {
+    $loading?: boolean;
+}
+
+export const Content = tw.div<ContentProps>`
+    ${p => p.$loading ? 'opacity-0' : 'opacity-100'}
 `
