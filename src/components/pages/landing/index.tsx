e3eb773c6277e7f90ad837e707e5674ce587dd40
Lucas <lucasfschraier@gmail.com>
2022-01-11 16:43:40 -0500

    Remove redundant code

--- src/components/pages/landing/index.tsx
+++ src/components/pages/landing/index.tsx
@@ -1,44 +1,7 @@
-import { useLoginMutation, useRegisterMutation } from '@/api/auth.api';
-import Button from '@/components/atoms/button';
 import Page from '@/components/atoms/page';
 import LoginForm from '@/components/organisms/login-form';
-import { login } from '@/reducers/session.reducer';
-import { handleInput } from '@/utils/component.utils';
-import Field from '@molecules/field';
-import { useCallback, useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import * as S from './styled';
 
 export default function Landing() {
-
-    const dispatch = useDispatch();
-
-    const [triggerLogin, {data, error, isLoading}] = useLoginMutation();
-
-    const [inputs, setInputs] = useState({
-        username: "",
-        password: ""
-    });
-
-    const handleLogin = useCallback( () => {
-        triggerLogin({
-            user: inputs
-        })
-        .unwrap()
-        .then( res => {
-            const {token, user} = res;
-            dispatch({
-                type: login.type,
-                payload: {
-                    jwt: token,
-                    id: user.id
-                }
-            })
-        })
-        .catch( err => {
-            console.log(err)
-        })
-    }, [inputs])
 
     return(
         <Page>
--- src/components/pages/register/index.tsx
+++ src/components/pages/register/index.tsx
@@ -1,45 +1,7 @@
-import { useRegisterMutation } from '@/api/auth.api';
-import Book from '@atoms/book';
-import Button from '@atoms/button';
-import { login } from '@/reducers/session.reducer';
-import { handleInput } from '@/utils/component.utils';
-import Field from '@molecules/field';
-import { useCallback, useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import * as S from './styled';
 import Page from '@/components/atoms/page';
 import RegisterForm from '@/components/organisms/register-form';
 
 export default function Register() {
-
-    const dispatch = useDispatch();
-
-    const [triggerRegister, {data, error, isLoading}] = useRegisterMutation();
-
-    const [inputs, setInputs] = useState({
-        username: "",
-        password: ""
-    });
-
-    const handleRegister = useCallback( () => {
-        triggerRegister({
-            user: inputs
-        })
-        .unwrap()
-        .then( res => {
-            const {token, user} = res;
-            dispatch({
-                type: login.type,
-                payload: {
-                    jwt: token,
-                    id: user.id
-                }
-            })
-        })
-        .catch( err => {
-            console.log(err)
-        })
-    }, [inputs])
 
     return(
         <Page>
