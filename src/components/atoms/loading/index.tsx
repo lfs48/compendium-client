05ca2e5dbc94e1e64bea6a270059938a263908c2
Lucas <lucasfschraier@gmail.com>
2022-01-15 13:17:14 -0500

    Replace loading page with loading atom

---
+++ src/components/atoms/loading/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styled';
+
+export default function Loading() {
+    return(
+        <S.Root>
+            <S.Spinner />
+        </S.Root>
+    )
+}
---
+++ src/components/atoms/loading/styled.ts
@@ -0,0 +1,16 @@
+import tw from 'tailwind-styled-components';
+import { Oval } from 'svg-loaders-react';
+
+export const Root = tw.div`
+    h-full
+    w-full
+    bg-gray-200
+    flex
+    justify-center
+    items-center
+`
+
+export const Spinner = tw(Oval)`
+    text-blue-500 
+    stroke-current
+`
