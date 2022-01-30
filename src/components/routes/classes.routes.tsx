dfb9e10eb398c2db4aa8a73a8aae3a7a7c22f5f3
Lucas <lucasfschraier@gmail.com>
2022-01-30 18:00:03 -0500

    Wait for features to load before rendering class routes

--- src/components/routes/classes.routes.tsx
+++ src/components/routes/classes.routes.tsx
@@ -2,17 +2,19 @@ import { useGetAllClassesQuery } from '@/api/dndclasses.api';
 import Loading from '@/components/atoms/loading';
 import { Route, Routes } from 'react-router-dom';
 import ClassForm from '@organisms/class-form';
+import { useGetAllFeaturesQuery } from '@/api/features.api';
 
 export default function ClassRoutes() {
 
     const getClassesQuery = useGetAllClassesQuery ();
+    const getFeaturesQuery = useGetAllFeaturesQuery();
 
     return(
         <Routes>
         <Route
             path='/classes/new'
             element={
-                getClassesQuery.isLoading
+                (getClassesQuery.isLoading || getFeaturesQuery.isLoading)
                     ?
                         <Loading />
                     :
@@ -22,7 +24,7 @@ export default function ClassRoutes() {
         <Route
             path='/classes/edit/:id'
             element={
-                getClassesQuery.isLoading
+                (getClassesQuery.isLoading || getFeaturesQuery.isLoading)
                     ?
                         <Loading />
                     :
