b7237c6fb0fc9905e549bbf9d89875c22766e7ea
Lucas <lucasfschraier@gmail.com>
2023-08-27 14:50:35 -0400

    feat: implement items api

---
+++ src/api/items.api.ts
@@ -0,0 +1,42 @@
+import { Item } from '@/types';
+import { PatchItem, PostItem } from '@/types/Item';
+import { rootApi } from './root.api';
+
+export const itemApi = rootApi.injectEndpoints({
+    endpoints: (builder) => ({
+        getAllItems: builder.query<Item[], void>({
+            query: () => `items`
+        }),
+        getItemById: builder.query<Item, string>({
+            query: (id) => `items/${id}`
+        }),
+        postItem: builder.mutation<Item, PostItem>({
+            query: ({...post}) => ({
+                url: `/items/${post.item.id}`,
+                method: 'POST',
+                body: post
+            })
+        }),
+        patchItem: builder.mutation<Item, PatchItem>({
+            query: ({...patch}) => ({
+                url: `/items/${patch.item.id}`,
+                method: 'PATCH',
+                body: patch
+            })
+        }),
+        deleteItem: builder.mutation<Item, string>({
+            query: (id) => ({
+                url: `/items/${id}`,
+                method: 'DELETE'
+            })
+        })
+    })
+});
+
+export const { 
+    useGetItemByIdQuery,
+    useGetAllItemsQuery,
+    usePostItemMutation,
+    usePatchItemMutation,
+    useDeleteItemMutation
+} = itemApi;
