ac40e544c613e19f01763cfb394e11ea8e0ac0ed
Lucas <lucasfschraier@gmail.com>
2023-10-07 21:33:36 -0400

    style: wrap app in styled component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import Routes from "@/components/routes"
 import Navbar from "@/components/concerns/navbar"
+import tw from "tailwind-styled-components"
+
+const Root = tw.div`
+  w-full 
+  h-full 
+  text-black 
+  dark:text-gray-200
+`
 
 function App() {
 
   return (
-    <>
-    <Navbar />
-    <Routes />
-    </>
+    <Root>
+      <Navbar />
+      <Routes />
+    </Root>
   )
 }
 
