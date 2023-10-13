22afd243b8b7393cc99878a709f71345952bcb8d
Lucas <lucasfschraier@gmail.com>
2023-10-13 15:30:08 -0400

    style: add bottom border to collection menu header

--- src/components/UI/navbar/collections/collection-menu-header/styled.ts
+++ src/components/UI/navbar/collections/collection-menu-header/styled.ts
@@ -3,6 +3,11 @@ import tw from 'tailwind-styled-components';
 export const Root = tw.div`
     flex 
     justify-between
+    border-b
+    border-gray
+    -mx-4
+    px-4
+    pb-1
 `
 
 export const Back = tw.i`
