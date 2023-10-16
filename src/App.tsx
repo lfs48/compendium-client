1dfa4af066bd4a3d08a7c1c37b512b9619cabaed
Lucas <lucasfschraier@gmail.com>
2023-10-16 16:48:35 -0400

    refactor: move appwide stylings from app wrapper component to index file root component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
 import Routes from "@/components/routes"
 import Navbar from "@/components/UI/navbar"
-import tw from "tailwind-styled-components"
 import Footer from "@/components/UI/footer"
 
-const Root = tw.div`
-  w-full 
-  h-full 
-  text-black 
-  dark:text-gray-200
-`
 
 function App() {
 
   return (
-    <Root>
+    <>
       <Navbar />
       <Routes />
       <Footer />
-    </Root>
+    </>
   )
 }
 
