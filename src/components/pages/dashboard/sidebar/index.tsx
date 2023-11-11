30bf2cae74c153e2464ac2b2db16e86537365dc2
Lucas <lucasfschraier@gmail.com>
2023-11-11 16:47:38 -0500

    feat: replace calls to all individual entity index routes with single call to entities index

--- src/components/pages/dashboard/sidebar/index.tsx
+++ src/components/pages/dashboard/sidebar/index.tsx
@@ -1,32 +1,22 @@
-import { useGetAllClassesQuery } from '@/api/dndclasses.api';
-import { useGetAllFeaturesQuery } from '@/api/features.api';
 import Loading from '@/components/UI/loading';
 import SidebarControls from '@/components/pages/dashboard/sidebar/sidebar-controls';
 import SidebarTabSelect from '@/components/pages/dashboard/sidebar/sidebar-tab-select';
 import { RootState } from '@/types';
 import { useSelector } from 'react-redux';
 import * as S from './styled';
-import { useGetAllRacesQuery } from '@/api/races.api';
 import { useRecoilState } from 'recoil';
 import { sidebarAtom } from '@/recoil';
 import SidebarContent from '@/components/pages/dashboard/sidebar/sidebar-content';
-import { useGetAllSpellsQuery } from '@/api/spells.api';
-import { useGetAllItemsQuery } from '@/api/items.api';
 import SidebarHide from './sidebar-hide';
 import { Entity } from '@/enums';
+import { useGetAllEntitiesQuery } from '@/api/entities.api';
 
 export default function Sidebar() {
 
     const [sidebarState, setSidebarState] = useRecoilState(sidebarAtom);
     const {selectedTab} = sidebarState;
 
-    const queries = {
-        dndClasses: useGetAllClassesQuery(),
-        features: useGetAllFeaturesQuery(),
-        races: useGetAllRacesQuery(),
-        spells: useGetAllSpellsQuery(),
-        items: useGetAllItemsQuery()
-    };
+    const { isSuccess } = useGetAllEntitiesQuery();
 
     const entities = useSelector( (state:RootState) => ({
         dndClasses: state.entities.dndClasses,
@@ -51,7 +41,7 @@ export default function Sidebar() {
             <S.Body>
                 <SidebarControls />
                 <S.Content>
-                {queries[selectedTab].isSuccess ?
+                {isSuccess ?
                     <SidebarContent />
                 :
                     <Loading />
--- src/components/pages/dashboard/workspace/index.tsx
+++ src/components/pages/dashboard/workspace/index.tsx
@@ -4,35 +4,23 @@ import FeatureRoutes from '@/components/routes/features.routes';
 import RaceRoutes from '@/components/routes/races.routes';
 import SpellRoutes from '@/components/routes/spells.routes';
 import * as S from './styled';
-import { useGetAllClassesQuery } from '@/api/dndclasses.api';
-import { useGetAllFeaturesQuery } from '@/api/features.api';
-import { useGetAllRacesQuery } from '@/api/races.api';
-import { useGetAllSpellsQuery } from '@/api/spells.api';
 import ItemRoutes from '@/components/routes/items.route';
-import { useGetAllItemsQuery } from '@/api/items.api';
+import { useGetAllEntitiesQuery } from '@/api/entities.api';
 
 export default function Workspace() {
 
-    const queries = {
-        dndClasses: useGetAllClassesQuery(),
-        features: useGetAllFeaturesQuery(),
-        races: useGetAllRacesQuery(),
-        spells: useGetAllSpellsQuery(),
-        items: useGetAllItemsQuery()
-    };
+    const {isSuccess} = useGetAllEntitiesQuery();
 
     return(
         <S.Root>
-            <ClassRoutes />
-            <RaceRoutes />
-            <FeatureRoutes />
-            <SpellRoutes />
-            <ItemRoutes />
-            {queries.dndClasses.isSuccess &&
-            queries.features.isSuccess &&
-            queries.races.isSuccess &&
-            queries.spells.isSuccess &&
-            queries.items.isSuccess
+            {isSuccess &&
+                <>
+                <ClassRoutes />
+                <RaceRoutes />
+                <FeatureRoutes />
+                <SpellRoutes />
+                <ItemRoutes />
+                </>
             }
         </S.Root>
     )
