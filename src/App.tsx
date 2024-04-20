73f078a87dad2aa3198b752b8301cfeb5664c54e
Lucas <lucasfschraier@gmail.com>
2024-04-20 16:44:15 -0400

    refactor: request & load user data at app root level

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import Routes from "@/components/routes"
 import tw from "tailwind-styled-components"
 import { Toaster } from 'react-hot-toast';
 import AppToaster from "./components/UI/app-toaster";
+import { useAppSelector } from "./hooks/useAppSelector.hook";
+import { useGetUserByIdQuery } from "./api/users.api";
+import { skipToken } from "@reduxjs/toolkit/query";
 
 const Root = tw.div`
   w-full 
@@ -11,6 +14,10 @@ const Root = tw.div`
 `
 
 function App() {
+
+  const id = useAppSelector( (state) => state.session.id)
+  
+  const { data } = useGetUserByIdQuery(id ?? skipToken);
 
   return (
     <Root id='app'>
--- src/components/UI/navbar/index.tsx
+++ src/components/UI/navbar/index.tsx
@@ -1,9 +1,6 @@
-import { useGetUserByIdQuery } from '@/api/users.api';
 import Logout from './logout';
-import { RootState } from '@/types';
 import { useAppSelector } from '@/hooks/useAppSelector.hook';
 import * as S from './styled';
-import { skipToken } from '@reduxjs/toolkit/query';
 import DarkMode from './darkmode';
 import Collections from './collections';
 import AuthLinks from './auth-links';
@@ -11,21 +8,20 @@ import AuthLinks from './auth-links';
 export default function Navbar() {
 
     const id = useAppSelector( (state) => state.session.id)
+    const user = useAppSelector( (state) => state.entities.users[id || ''])
     const authenticated = !!id;
-    
-    const { data } = useGetUserByIdQuery(id ?? skipToken);
 
     return(
         <S.Root>
             <S.Left>
-                {(authenticated && data) &&
+                {(authenticated && user) &&
                     <Collections />
                 }
             </S.Left>
             <S.Right>
-                {(authenticated && data) ?
+                {(authenticated && user) ?
                     <>
-                    <span>{data.username}</span>
+                    <span>{user.username}</span>
                     <DarkMode />
                     <Logout />
                     </>
